ad277f76cd82fd59cc8b856fbbf13375d8b97351
kuzeydurden <hidden@kuzeydurden.local>
2025-11-22 16:08:08 +0300

    refactor: replace Lucide Twitter icon with custom XIcon component

--- components/profile-section.tsx
+++ components/profile-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion, useMotionValue, useSpring, useTransform } from "framer-motion"
-import { Github, Send, Twitter } from "lucide-react"
+import { Github, Send } from "lucide-react"
 import Link from "next/link"
 import { MouseEvent, useRef } from "react"
 
@@ -66,7 +66,7 @@ export function ProfileSection() {
                         className="mt-6 flex space-x-4"
                     >
                         <SocialLink href="https://github.com/0xkuzeydurden" icon={<Github className="h-6 w-6" />} label="GitHub" />
-                        <SocialLink href="https://x.com/islakwcterlii/" icon={<Twitter className="h-6 w-6" />} label="X" />
+                        <SocialLink href="https://x.com/islakwcterlii/" icon={<XIcon className="h-5 w-5 fill-current" />} label="X" />
                         <SocialLink href="https://t.me/kuzeydurdeneth" icon={<Send className="h-6 w-6" />} label="Telegram" />
                     </motion.div>
                 </div>
@@ -86,5 +86,13 @@ function SocialLink({ href, icon, label }: { href: string; icon: React.ReactNode
             <span className="sr-only">{label}</span>
             {icon}
         </Link>
+    )
+}
+
+function XIcon({ className }: { className?: string }) {
+    return (
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className}>
+            <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+        </svg>
     )
 }
